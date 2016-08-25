20f316a57cf32325326a95b195d481962dff075e
xinbeiji <xinbeiji@yeah.net>
2016-08-25 09:16:09 +0800

    Initial Commit

---
+++ NewsSocial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsSocial
+//
+//  Created by ZYJY on 16/8/25.
+//  Copyright © 2016年 com.ZYJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewsSocial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewsSocial
+//
+//  Created by ZYJY on 16/8/25.
+//  Copyright © 2016年 com.ZYJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
